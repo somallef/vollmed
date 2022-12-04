44b3d0261f66aa81b707ad2aa7c0d9b949e40deb
Allef Mizael <allef.mizael@gmail.com>
2022-12-03 23:16:00 -0300

    AULA 3: Adição das dependências para persistência no banco de dados; Criação das classes JPA e seus mapeamentos; Criação do repository; Criação da primeira Migration

--- src/main/java/med/voll/VollMedApi/controller/MedicoController.java
+++ src/main/java/med/voll/VollMedApi/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package med.voll.VollMedApi.controller;
 
 import med.voll.VollMedApi.medico.DadosCadastroMedico;
+import med.voll.VollMedApi.medico.Medico;
+import med.voll.VollMedApi.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
 
     }
 }
