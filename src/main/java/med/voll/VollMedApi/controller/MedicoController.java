da6b2caf91bf247208b71f5d5d181892bfc6f21a
Allef Mizael <allef.mizael@gmail.com>
2022-12-04 10:56:55 -0300

    CURSO1/AULA4/COMMIT1: Criação do método de listagem de médicos

--- src/main/java/med/voll/VollMedApi/controller/MedicoController.java
+++ src/main/java/med/voll/VollMedApi/controller/MedicoController.java
@@ -2,14 +2,14 @@ package med.voll.VollMedApi.controller;
 
 import jakarta.validation.Valid;
 import med.voll.VollMedApi.medico.DadosCadastroMedico;
+import med.voll.VollMedApi.medico.DadosListagemMedico;
 import med.voll.VollMedApi.medico.Medico;
 import med.voll.VollMedApi.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -26,5 +26,13 @@ public class MedicoController {
             DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
 
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar() {
+        return repository.findAll() //retorna uma lista de médicos
+                .stream() //cria um fluxo/pipeline de transformação que será executado pelo map
+                .map(DadosListagemMedico::new) //instancia um obj de listagem e envia o list de médicos para o seu construtor
+                .toList(); //converte para o tipo lista
     }
 }
