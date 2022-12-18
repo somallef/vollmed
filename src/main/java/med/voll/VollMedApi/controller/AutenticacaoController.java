edcf5622c0680a9d3d47373dfbe809b4cae00c94
Allef Mizael <allef.mizael@gmail.com>
2022-12-18 18:58:18 -0300

    Aqui foi implementada a funcionalidade de retorno de token JWT após a autenticação

--- src/main/java/med/voll/VollMedApi/controller/AutenticacaoController.java
+++ src/main/java/med/voll/VollMedApi/controller/AutenticacaoController.java
@@ -2,6 +2,9 @@ package med.voll.VollMedApi.controller;
 
 import jakarta.validation.Valid;
 import med.voll.VollMedApi.domain.usuario.DadosAutenticacao;
+import med.voll.VollMedApi.domain.usuario.Usuario;
+import med.voll.VollMedApi.infra.security.DadosTokenJWT;
+import med.voll.VollMedApi.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,16 +20,20 @@ public class AutenticacaoController {
 
     @Autowired
     private AuthenticationManager authManager;
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping
     public ResponseEntity login(
             @RequestBody
             @Valid
             DadosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha()); //passamos os dados do dto do dominio da aplicação para que o spring crie o seu proprio dto
-        var auth = authManager.authenticate(token); //passamos o token (dto do spring) para que seja realizada a autenticação
+        var authToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha()); //passamos os dados do dto do dominio da aplicação para que o spring crie o seu proprio dto
+        var auth = authManager.authenticate(authToken); //passamos o token (dto do spring) para que seja realizada a autenticação
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.gerarToken((Usuario) auth.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
 
     }
 }
---
+++ src/main/java/med/voll/VollMedApi/infra/security/DadosTokenJWT.java
@@ -0,0 +1,4 @@
+package med.voll.VollMedApi.infra.security;
+
+public record DadosTokenJWT(String token) {
+}
